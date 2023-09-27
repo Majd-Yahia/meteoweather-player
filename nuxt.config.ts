39f8b78397d46ad6527565f3b3a2d877ee59cf64
Majd Yahia <majd.m4a4@gmail.com>
2023-09-27 21:04:59 +0300

    fixing some packages and cleaning up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", 'nuxt-mdi'],
+  app: {
+    head: {
+      script: [
+        // { src: '/js/jquery.min.js', type: 'text/javascript' },
+      ],
+    },
+  },
+  // Not working, need to find out why.
+  mdi: {
+    cache: false,
+    componentName: 'MdiIcon',
+    defaultSize: '5rem'
+  },
 })
